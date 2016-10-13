d913bd76cde7be743999c48840f020b0923eb81f
Fernando Trigoso <fertrig@gmail.com>
2016-10-12 21:53:51 -0400

    fixing port

--- src/test-server.js
+++ src/test-server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
@@ -9,6 +8,6 @@ const server = http.createServer((req, res) => {
 	res.end('Hello World - \n');
 });
 
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+	console.log(`Server running at port ${port}.`);
 });
