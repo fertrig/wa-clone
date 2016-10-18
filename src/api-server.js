02fcd8c6053a735b48a813e0ae7ce914613d254d
Fernando Trigoso <fertrig@gmail.com>
2016-10-17 23:08:52 -0400

    redis setup, redis test route

--- src/api-server.js
+++ src/api-server.js
@@ -1,13 +1,33 @@
 import express from 'express';
+import {setupRedis, redisClient} from './redis-client';
+
+if (!process.env.PORT) {
+	require('dotenv').load();
+	console.log('.env loaded manually, port', process.env.PORT);
+}
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(port, () => {
-	console.log('Express app listening on port 3000');
+app.get('/redis-test', (req, res) => {
+	redisClient.incr('inc-test', (err, result) => {
+		if (err) {
+			res.send('Error incrementing redis test');
+		}
+		else {
+			res.send(`Redis increment test result: ${result}`);
+		}
+	})
 });
+
+setupRedis();
+
+app.listen(port, () => {
+	console.log('express app listening on port 3000');
+});
+
---
+++ src/redis-client.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+let client = null;
+
+function setup() {
+	if (process.env.REDISCLOUD_URL) {
+		console.log('connecting to redis cloud');
+		client = redis.createClient(process.env.REDISCLOUD_URL);
+	}
+	else {
+		console.log('connecting to local redis');
+		client = redis.createClient();
+	}
+
+	client.on('connect', function() {
+		console.log('redis client emitted connect event');
+	});
+
+	client.on('error', function (err) {
+		console.error(err, 'redis error');
+	});
+}
+
+export {setup as setupRedis, client as redisClient};
